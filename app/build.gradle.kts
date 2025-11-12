063ff77d5f662ee30ad48a1a0b6adc073b0d527e
juraj <xlopusek@stuba.sk>
2025-11-12 13:04:12 +0100

    CPG-20: add DFA detection logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         targetSdk = 36
 
         // Major * 10000 + Minor * 100 + Patch
-        versionCode = 20001
+        versionCode = 20002
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
