336b11abb0a1bc1858f770a429ed8b1e565d49b1
Juraj Lopušek <lopusekjuraj@webgaleria.sk>
2025-12-09 18:23:59 +0100

    CPG-20: add DFA button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         targetSdk = 36
 
         // Major * 10000 + Minor * 100 + Patch
-        versionCode = 20002
+        versionCode = 20003
         versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
